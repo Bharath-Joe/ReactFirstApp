1a39dcba0cbf4a7fb90f93abd9671fc8da042778
Bharath <bsenthil@calpoly.edu>
2021-09-24 13:31:08 -0700

    part 3

--- src/Table.js
+++ src/Table.js
@@ -15,9 +15,13 @@ function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>         
+          <td>{row.name}</td>
           <td>{row.job}</td>
-        </tr>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete
+          </button>
+            </td>
+          </tr>
       );
      }
     );
@@ -29,12 +33,12 @@ function TableBody(props) {
   }
   
   
-function Table(props) {
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={props.characterData} />
-        </table>
-      )
-}
+  function Table (props) {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+      </table>
+    );
+  }
   export default Table;
